dbf7bc10e992ee95de1620593b3d02a6bf78a0c0
Patrick1014 <pzxy1014@gmail.com>
2025-02-13 11:45:46 +0000

    Added about page template and fixed index linking

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,7 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name='index'), #/rango/
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'), #/rango/index
     path('about/', views.about, name='about'), #/rango/about/
 ]
