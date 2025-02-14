a591db56b6f874047c9a038066ba77c9d4008c14
Patrick1014 <pzxy1014@gmail.com>
2025-02-14 13:21:38 +0000

    Fixed add_page issue and finish chapter 7

--- rango/urls.py
+++ rango/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('index/', views.index, name='index'), #/rango/index/
     path('about/', views.about, name='about'), #/rango/about/
-    path('category/<slug:category_name_slug>/',
-         views.show_category, name='show_category'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
+    path('add_category/', views.add_category, name='add_category'),
+
+
 ]
