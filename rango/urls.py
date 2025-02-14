8785ad3a4dde3b064f08ac0b1924a01efeb041a2
Patrick1014 <pzxy1014@gmail.com>
2025-02-14 10:07:41 +0000

    Updatedchapter 5 and chapter 6 category, views, templates, and migrations

--- rango/urls.py
+++ rango/urls.py
@@ -5,6 +5,8 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index/', views.index, name='index'), #/rango/index
+    path('index/', views.index, name='index'), #/rango/index/
     path('about/', views.about, name='about'), #/rango/about/
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name='show_category'),
 ]
